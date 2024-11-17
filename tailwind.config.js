9bbd884ff2cc31263f4de7a98cba705cd2d88cb3
Marlon Graichy <mgraichy@gmail.com>
2024-11-17 13:35:41 -0500

    work on header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vue: 'rgb(var(--color-vue))',
+      },
+    },
   },
   plugins: [],
 }
