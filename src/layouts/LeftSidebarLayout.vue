20677673a506a2098cff09ceb2a4b7ded977b99a
Marlon Graichy <mgraichy@gmail.com>
2024-11-19 08:00:56 -0500

    ot the grid working

--- src/layouts/LeftSidebarLayout.vue
+++ src/layouts/LeftSidebarLayout.vue
@@ -1,15 +1,25 @@
 <script setup>
-    import { inject, ref } from 'vue';
+    import { inject } from 'vue';
+    // import MenuIcon from 'vue-material-design-icons/Menu.vue';
+    import Home from 'vue-material-design-icons/Home.vue';
     const leftSidebar = inject('leftSidebar');
 </script>
 
 <template>
-        <div class="flex flex-col">LEFT SIDEBAR
-            <div v-if="leftSidebar">HERE I AM!</div>
-            <div v-else>THERE YOU ARE!</div>
-            <div v-if="leftSidebar">HERE I AM!</div>
-            <div v-else>THERE YOU ARE!</div>
-            <div v-if="leftSidebar">HERE I AM!</div>
-            <div v-else>THERE YOU ARE!</div>
-        </div>
+    <ul class="flex flex-col py-2">
+        <li>
+        <!-- <button class="btn-glow large-screen-extra-btn" @click="leftSidebar = !leftSidebar">
+            <MenuIcon />
+        </button> -->
+        </li>
+        <li>
+            <RouterLink
+                :to="{name: 'home'}"
+                class="flex flex-row gap2 justify-between"
+            >
+                <Home />
+                <div v-if="leftSidebar" class="align-self">Home</div>
+            </RouterLink>
+        </li>
+    </ul>
 </template>
