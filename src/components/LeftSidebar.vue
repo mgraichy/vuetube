4b44ae352fd2b307a79038a7bea100bf9c24f106
Marlon Graichy <mgraichy@gmail.com>
2024-11-19 20:30:08 -0500

    fix single video view
    * fix back / forward buttons

---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { inject } from 'vue';
+    import Home from 'vue-material-design-icons/Home.vue';
+    import LeftSidebarLayout from '../layouts/LeftSidebarLayout.vue';
+
+    const leftSidebar = inject('leftSidebar');
+</script>
+
+
+<template>
+    <LeftSidebarLayout>
+        <li>
+            <RouterLink
+                :to="{name: 'home'}"
+                class="flex flex-row gap2 justify-between"
+            >
+                <Home />
+                <div v-if="leftSidebar" class="align-self">Home</div>
+            </RouterLink>
+        </li>
+    </LeftSidebarLayout>
+</template>
--- src/layouts/IndividualVideoLayout.vue
+++ src/layouts/IndividualVideoLayout.vue
@@ -1,45 +1,60 @@
 <script setup>
-    import { computed, inject, ref } from 'vue';
+    import { computed, inject, ref, watch } from 'vue';
     import { useRoute, useRouter } from 'vue-router';
     import { formatTimeAgo } from '../utils/formatTimeAgo.js';
     const domain = import.meta.env.VITE_DOMAIN;
 
     const videoArray = inject('videoArray');
-    const router = useRouter();
     const route = useRoute();
-    const mainVideo = JSON.parse(route.query.vid);
+    const router = useRouter();
 
-    // console.log(mainVideo);
+    const mainVideo = ref(JSON.parse(route.query.vid || '{}'));
 
     function goToVideo(vid) {
         router.push({
             name: 'watch',
-            params: { id: vid.id, },
-            query: { vid: JSON.stringify(vid) }
+            params: { id: vid.id },
+            query: { vid: JSON.stringify(vid) },
         });
+
+        // Update mainVideo without modifying videoArray.
+        mainVideo.value = vid;
     }
+
+    watch(
+        () => route.query.vid,
+        (newMainVid) => {
+            mainVideo.value = JSON.parse(newMainVid);
+        }
+    );
+
+    const separateSidebarVideos = computed(() => {
+        // Exclude mainVideo from sidebar:
+        return videoArray.value.filter(video => video.id !== mainVideo.value.id);
+    });
 </script>
 
 <template>
-    <div v-if="videoArray"
-        class="grid
-            gap-4
-            grid-cols-2
-            auto-rows-min
-            overflow-y-auto"
-    >
-        <div>
-            <video :src="domain+mainVideo.src" type="video/mp4"></video>
+    <div v-if="videoArray" class="grid grid-cols-[auto,1fr] flex-grow-1 overflow-y-auto">
+        <div class="flex">
+            <div class="cursor-pointer aspect-video">
+                <video :src="domain + mainVideo.src" type="video/mp4" controls></video>
+            </div>
         </div>
-        <div class="grid grid-cols-subgrid gap-4">
-            <!-- <div v-for="(vid, index) in videoArray"
-                :key="index"
-                @click="goToVideo(vid)"
-                class="cursor-pointer aspect-video"
-            >
-                <!-- <div><pre>{{ videoArray[0] }}</pre></div>
-                <video :src="domain+vid.src" type="video/mp4"></video>
-            </div> -->
+        <div id="right-sidebar" class="flex-col"> <!-- grid grid-cols-[auto,.5fr] -->
+            <div class="flex">
+                <div
+                    v-for="(vid, index) in separateSidebarVideos"
+                    :key="index"
+                    @click="goToVideo(vid)"
+                    class="cursor-pointer aspect-video"
+                >
+                    <!-- <div class="border-2 border-white border-solid"> -->
+                        <video :src="domain + vid.src" type="video/mp4"></video>
+                    <!-- </div> -->
+                    <div>HEY</div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
