412cf8ee777783663f3a9be7fa6b9bc838a0ad68
Marlon Graichy <mgraichy@gmail.com>
2024-12-09 19:58:30 -0500

    got the text not showing before the actual videos load up
        -- This had to do with:
            -- a) an async function "getVideoBlob()" which would await goFetchVideo(), and
            -- b) the totalLength within getVideoBlob() --- if it was equal to the
                  currentLength (both ref variables), then print out the text

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,7 +8,11 @@
     const videoStringArray = inject('videoStringArray');
     const videoUrl = `${config.oauthUri}/api/videos`;
 
-    const loaded = ref(false);
+    // Preventing the text from loading before the videos have loaded,
+    // see also getVideoBlob():
+    const printText = ref(false);
+    const totalLength = ref(0);
+    const currentLength = ref(0);
 
     const router = useRouter();
 
@@ -20,10 +24,19 @@
         });
     }
 
-    function getVideoBlob(vid, id) {
-        const url = `${videoUrl}?file=${vid.src}`;
-        goFetchVideo(url, id);
+    function getTotalLength() {
+        totalLength.value = videoStringArray.value.length;
     }
+
+    async function getVideoBlob(vid, id) {
+        const url = `${videoUrl}?file=${vid.src}`;
+        // Preventing the text from loading before the videos have loaded:
+        await goFetchVideo(url, id);
+        currentLength.value++;
+        if (totalLength.value == currentLength.value) {
+            printText.value = true;
+        }
+    };
 </script>
 
 <template>
@@ -34,6 +47,7 @@
                 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]
                 auto-rows-min"
         >
+            {{ getTotalLength() }}
             <div v-for="(vid, index) in videoStringArray"
                 :key="index"
                 @click="goToVideo(vid)"
@@ -56,7 +70,7 @@
                     >
                     Your browser does not support the video tag.
                 </video>
-                <div class="flex m-2">
+                <div v-show="printText" class="flex m-2">
                     <div class="rounded-full self-center h-12 w-12 bg-vue mr-4 text-center leading-[3rem]">
                         {{ vid.initials }}
                     </div>
