a0363c0da0faecf6bec929edd7b009f60fefc807
Marlon Graichy <mgraichy@gmail.com>
2024-12-10 14:52:10 -0500

    Working IndividualVideo.vue
        -- had to make the ids for <source> different in right sidebar
           between large and small screens

--- src/App-WITH-COMPUTED.vue
+++ src/App-WITH-COMPUTED.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { provide, ref } from 'vue';
+    import { computed, provide, ref } from 'vue';
     import { RouterView } from 'vue-router'
     import { config } from './composables/oauth2-config.js';
     import { redirectToAuthorizationServer } from './composables/oauth2-redirect.js';
@@ -8,16 +8,24 @@
     import LeftSidebar from './components/LeftSidebar.vue';
     import Loader from './components/Loader.vue';
 
+    // const accessToken = sessionStorage.getItem('access_token');
     const videoStringUrl = `${config.oauthUri}/api/video-strings?client_id=${config.clientId}`;
     const videoStringArray = ref([{}]);
     const pathname = ref(window.location.pathname);
-    const isLoading = ref(false);
-    goFetch(videoStringUrl, videoStringArray, isLoading);
+    goFetch(videoStringUrl, videoStringArray);
 
     const toggleLeftSidebar = ref(true);
     provide('leftSidebar', toggleLeftSidebar);
     provide('pathname', pathname);
     provide('videoStringArray', videoStringArray);
+
+    const isErrorInVideoStringArray = computed(() => {
+        return videoStringArray[0]?.error !== undefined;
+    });
+
+    const videoStringArrayStatus401 = computed(() => {
+        return videoStringArray[0]?.status == 401;
+    });
 
     var numberOfReloads = 0;
     function reloadPageCount() {
@@ -27,12 +35,12 @@
 </script>
 
 <template>
-    <!-- <div>Number of Reloads: {{ reloadPageCount() }}</div> -->
+    <div>Number of Reloads: {{ reloadPageCount() }}</div>
     <div class="p-0 m-0 h-svh flex flex-col overflow-hidden">
         <Header :video-string-array="videoStringArray"></Header>
-        <div v-if="isLoading"><Loader></Loader></div>
-        <div v-if="videoStringArray[0]?.error !== undefined">
-            <div v-if="pathname == '/' && videoStringArray[0]?.status == 401"
+        <div v-if="isErrorInVideoStringArray">
+            <!-- videoStringArray, ERROR: {{ videoStringArray }} -->
+            <div v-if="pathname == '/' && videoStringArrayStatus401"
                 class="grid justify-items-center items-center h-svh text-lg font-bold"
                 @click="redirectToAuthorizationServer()"
             >
